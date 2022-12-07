8970de5cde40d8853747fe75af16f602c93be072
Riddle <samuel.riddle1@gmail.com>
2022-12-06 21:09:08 -0700

    Implemented basic Escrow contract

--- contract/src/escrow.rs
+++ contract/src/escrow.rs
@@ -1,5 +1,56 @@
-use near_sdk::{ call, LookupMap, NearBindgen, view, assert, near, UnorderedMap, NearPromise };
+use near_sdk::{
+    env,
+    near_bindgen,
+    AccountId,
+    Balance,
+};
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Escrow {}
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct EscrowContract {
+    escrow_amount: Balance,
+    sender: AccountId,
+    receiver: AccountId,
+    arbiter: AccountId,
+    finalized: bool,
+}
+
+#[near_bindgen]
+impl EscrowContract {
+    pub fn new(escrow_amount: Balance, sender: AccountId, receiver: AccountId, arbiter: AccountId) -> Self {
+        Self { 
+            escrow_amount,
+            sender,
+            receiver,
+            arbiter,
+            finalized: false
+        }
+    }
+
+    pub fn release_funds(&mut self) {
+        assert!(env::predecessor_account_id() == self.arbiter, "Only the arbiter can release funds.");
+        assert!(!self.finalized, "Funds have already been released.");
+        self.finalized = true;
+        env::transfer_to_account(self.receiver, self.escrow_amount);
+    }
+
+    pub fn get_escrow_amount(&self) -> Balance {
+        self.escrow_amount
+    }
+
+    pub fn get_sender(&self) -> AccountId {
+        self.sender
+    }
+
+    pub fn get_receiver(&self) -> AccountId {
+        self.receiver
+    }
+
+    pub fn get_arbiter(&self) -> AccountId {
+        self.arbiter
+    }
+
+    pub fn is_finalized(&self) -> bool {
+        self.finalized
+    }
+}
